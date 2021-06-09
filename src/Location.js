e21e8467f1d7e998e1fb00b1f4c277c73802ea6f
Aaronsabu <70811901+Aaronsabu@users.noreply.github.com>
2021-06-09 08:10:34 +0530

    Add files via upload

---
+++ src/Location.js
@@ -0,0 +1,25 @@
+//import './_mockLocation';
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import Map from './map';
+
+const Location = () => {
+
+    return (
+       <View style={styles.container}>
+         <Map />
+       </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 100,
+        alignItems: 'center',
+        
+    },
+});
+
+export default Location;
+
+//{errorMsg ? <Text>Please enable location services</Text> : null}
